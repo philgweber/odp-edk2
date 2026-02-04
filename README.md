6a60f95900caa49b7ac208d889f9f3910cf9824b
Phil Weber <philweber@microsoft.com>
2026-02-03 16:44:16 -0800

    Added QEMU compile support and Dev Container

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # odp-edk2
+
+This repository contains the ODP edk2-based platform sources and build scripts.
+
+**Dev Container**
+
+- Open in VS Code and run **Remote-Containers: Reopen in Container** to use the provided devcontainer.
+- To build the container image manually and open a shell:
+
+**Run a platform build (example):**
+
+```bash
+cd ../edk2-odp/Platform/Qemu/SbsaQemu
+./build.sh
+```
+
+**Troubleshooting**
+
+- If tools are missing, confirm you're in the devcontainer shell or that required toolchains are installed.
+- For interactive edk2 setup steps, run `./edk2/edksetup.sh` manually and follow prompts.
--- edk2-odp/Platform/Qemu/SbsaQemu/build.sh
+++ edk2-odp/Platform/Qemu/SbsaQemu/build.sh
@@ -3,6 +3,7 @@
 # Setup environment variables
 export PLATFORM_ROOT=$PWD
 export WORKSPACE=$PLATFORM_ROOT/../../../..
+export QEMU_ROOT=$WORKSPACE/qemu
 export GCC5_AARCH64_PREFIX=/usr/bin/aarch64-linux-gnu-
 export PACKAGES_PATH=$WORKSPACE/edk2:$WORKSPACE/edk2-platforms:$WORKSPACE/edk2-non-osi
 
@@ -12,6 +13,11 @@ git apply --check $WORKSPACE/edk2-odp/Platform/Qemu/SbsaQemu/diffs/SbsaQemuHardw
 
 make -C $WORKSPACE/edk2/BaseTools
 source $WORKSPACE/edk2/edksetup.sh
+
+# Build QEMU
+cd $QEMU_ROOT
+./configure --target-list=aarch64-softmmu --enable-gtk
+ninja -C build
 
 # Build TF-A images
 cd $WORKSPACE/trusted-firmware-a
@@ -28,4 +34,4 @@ cp $WORKSPACE/Build/SbsaQemu/DEBUG_GCC5/FV/SBSA_FLASH[01].fd $PLATFORM_ROOT/outp
 truncate -s 256M $PLATFORM_ROOT/output/SBSA_FLASH[01].fd
 
 # Run QEMU
-qemu-system-aarch64 -machine sbsa-ref -cpu max -m 4G -drive if=pflash,format=raw,file=$PLATFORM_ROOT/output/SBSA_FLASH0.fd -drive if=pflash,format=raw,file=$PLATFORM_ROOT/output/SBSA_FLASH1.fd -serial stdio
+$QEMU_ROOT/build/qemu-system-aarch64 -machine sbsa-ref -cpu max -m 4G -drive if=pflash,format=raw,file=$PLATFORM_ROOT/output/SBSA_FLASH0.fd -drive if=pflash,format=raw,file=$PLATFORM_ROOT/output/SBSA_FLASH1.fd -serial stdio
